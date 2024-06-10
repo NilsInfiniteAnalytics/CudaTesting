987b82a925303e379a724ee8407e9b3d43bdfdf6
Christopher Popovich <c.a.popovich@gmail.com>
2024-06-09 21:06:44 -0400

    Add a multiplication kernel for testing. naming.

--- CudaTesting.cpp
+++ CudaTesting.cpp
@@ -6,6 +6,10 @@ int main()
 	double a[3], b[3], c[3];
 	a[0] = 1.0; a[1] = 2.0; a[2] = 3.0;
 	b[0] = 4.0; b[1] = 5.0; b[2] = 6.0;
-	kernel(a, b, c, 3);
-	std::cout << "C = " << c[0] << ", " << c[1] << ", " << c[2] << '\n';
+	vector_addition_kernel_wrapper_kernel(a, b, c, 3);
+	std::cout << "C (Addition) = " << c[0] << ", " << c[1] << ", " << c[2] << '\n';
+	vector_multiplication_kernel_wrapper_kernel(a, b, c, 3);
+	std::cout << "C (Multiplication) = " << c[0] << ", " << c[1] << ", " << c[2] << '\n';
+
+	return 0;
 }
--- cuda_kernel.cuh
+++ cuda_kernel.cuh
@@ -1,1 +1,2 @@
-void kernel(double* a, double* b, double* c, int array_size);
+void vector_addition_kernel_wrapper_kernel(double* a, double* b, double* c, int array_size);
+void vector_multiplication_kernel_wrapper_kernel(double* a, double* b, double* c, int array_size);
